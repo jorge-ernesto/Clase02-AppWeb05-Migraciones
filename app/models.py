d609a802bdf570fc9eeeb60aebd5a2eeb9f800f4
Ernesto <jorge.cywdt@gmail.com>
2023-04-06 12:56:54 -0500

    Cambios realizados por SISTEMASUNI
    ---- Gitignore ----
    
    - Gitignore for a Django project
    https://djangowaves.com/tips-tricks/gitignore-for-a-django-project/
    
    - Permitimos SQLite3 en Gitignore

---
+++ app/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+        
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    available = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    
+    def __str__(self): 
+        return self.name
