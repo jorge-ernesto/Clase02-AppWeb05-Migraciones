0753d511ae0626cf9afd80a56fce96ada6a835db
Ernesto <jorge.cywdt@gmail.com>
2023-04-15 13:52:27 -0500

    Notas para entender mejor el codigo

--- app/models.py
+++ app/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 
 # Create your models here.
-class Category(models.Model):
+
+class Category(models.Model):  # Modelo creado para ejecutar migraciones
+    # Definir los datos de la clase (campos de la tabla)
+    # Si no se especifica lo contrario, todos los campos seran requeridos por defecto, es decir NOT NULL
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
         
-class Book(models.Model):
+class Book(models.Model):  # Modelo creado para ejecutar migraciones
+    # Definir los datos de la clase (campos de la tabla)
+    # Si no se especifica lo contrario, todos los campos seran requeridos por defecto, es decir NOT NULL
     name = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     available = models.BooleanField(default=True)
